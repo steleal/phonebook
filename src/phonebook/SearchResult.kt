266695935ce692418789dbbddf31298416368c8f
steleal <steleal@yandex.ru>
2020-01-30 20:43:53 +0400

    Update SearchResult.kt

--- src/phonebook/SearchResult.kt
+++ src/phonebook/SearchResult.kt
@@ -1,7 +1,7 @@
 package phonebook
 
-class SearchResult(var found: Int = 0, var all: Int = 0) {
-    override fun toString(): String {
-        return "Found $found / $all entries."
-    }
-}
+class SearchResult(
+        val found: Int,
+        val all: Int,
+        val millisec: Long
+)
