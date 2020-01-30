e3c7cb0d66484ba948c91f080e584b2fb69714a3
steleal <steleal@yandex.ru>
2020-01-30 21:23:25 +0400

    Implemented fun bubbleSortWithStop

--- src/phonebook/BubbleSort.kt
+++ src/phonebook/BubbleSort.kt
@@ -1,7 +1,35 @@
 package phonebook
 
-internal fun bubbleSortWithStop(lines: List<String>, maxTimeInMs: Long): Boolean {
+internal fun bubbleSortWithStop(lines: MutableList<String>, maxTimeInMs: Long): Boolean {
+
+    val startTime = System.currentTimeMillis()
+    for (i in lines.indices){
+        for (j in 1..(lines.lastIndex-i)){
+            val prev = j-1
+            if (lines[prev].isMoreThan(lines[j])){
+                change(lines, prev, j)
+            }
+        }
+        val time = System.currentTimeMillis() - startTime
+        if (time > maxTimeInMs) return true
+    }
 
     return false
+}
+
+private fun change(lines: MutableList<String>, i: Int, j: Int) {
+    val tmp = lines[i]
+    lines[i] = lines[j]
+    lines[j] = tmp
+}
+
+private inline fun String.isMoreThan(line: String): Boolean {
+    val thisNameStart = this.indexOfFirst { it ==' ' } + 1
+    val secondNameStart = line.indexOfFirst { it==' ' } + 1
+
+    val thisName = this.substring(thisNameStart)
+    val secondName = line.substring(secondNameStart)
+
+    return thisName.compareTo(secondName) > 0
 }
 
