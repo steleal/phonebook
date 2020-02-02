962b278d4eecb8b8307d9203cca936bdadaee503
steleal <steleal@yandex.ru>
2020-02-02 12:46:30 +0400

    Move fun millisecToString from Utils to Main

--- src/phonebook/Utils.kt
+++ src/phonebook/Utils.kt
@@ -1,9 +1,2 @@
 package phonebook
 
-internal fun Long.millisecToString(): String {
-    val ms = this % 1000
-    val allsec = this / 1000
-    val sec = allsec % 60
-    val min = allsec / 60
-    return "$min min. $sec sec. $ms ms."
-}
