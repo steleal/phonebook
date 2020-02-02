f13292dbb8c20798ae7e6e996dceaf9ed125f933
steleal <steleal@yandex.ru>
2020-02-02 13:35:38 +0400

    Moved fun jumpSearch to Search.kt

--- src/phonebook/JumpSearch.kt
+++ src/phonebook/JumpSearch.kt
@@ -1,24 +1,2 @@
 package phonebook
 
-internal fun jumpSearch(records: List<Record>, searchName: String): Record? {
-    if (records.isEmpty()) return null
-
-    val blockSize = Math.sqrt(records.size.toDouble()).toInt()
-    var index = 0
-
-    while (index < records.size) {
-        if (records[index].name == searchName) break
-        if (records[index].name < searchName) {
-            index += blockSize
-        }
-    }
-
-    if (index > records.lastIndex) index = records.lastIndex
-    if (records[index].name < searchName) return null
-
-    while (index >= 0 && records[index].name >= searchName) {
-        index--
-    }
-    index++
-    return if (records[index].name!=searchName) null else records[index]
-}
--- src/phonebook/Search.kt
+++ src/phonebook/Search.kt
@@ -5,3 +5,26 @@ internal fun linearSearch(records: List<Record>, searchName: String): Record? {
         record.name == searchName
     }
 }
+
+internal fun jumpSearch(records: List<Record>, searchName: String): Record? {
+    if (records.isEmpty()) return null
+
+    val blockSize = Math.sqrt(records.size.toDouble()).toInt()
+    var index = 0
+
+    while (index < records.size) {
+        if (records[index].name == searchName) break
+        if (records[index].name < searchName) {
+            index += blockSize
+        }
+    }
+
+    if (index > records.lastIndex) index = records.lastIndex
+    if (records[index].name < searchName) return null
+
+    while (index >= 0 && records[index].name >= searchName) {
+        index--
+    }
+    index++
+    return if (records[index].name!=searchName) null else records[index]
+}
