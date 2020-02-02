7eff49c918febf5421b3f7a2ac68ea5153f8a61d
steleal <steleal@yandex.ru>
2020-02-02 13:29:33 +0400

    Refactored BubbleSort

--- src/phonebook/BubbleSort.kt
+++ src/phonebook/BubbleSort.kt
@@ -1,35 +1,23 @@
 package phonebook
 
-internal fun bubbleSortWithStop(lines: MutableList<String>, maxTimeInMs: Long): Boolean {
+internal fun bubbleSort(records: MutableList<Record>) {
 
-    val startTime = System.currentTimeMillis()
-    for (i in lines.indices) {
-        for (j in 1..(lines.lastIndex - i)) {
+    for (i in records.indices) {
+        for (j in 1..(records.lastIndex - i)) {
             val prev = j - 1
-            if (lines[prev].isMoreThan(lines[j])) {
-                change(lines, prev, j)
+            if (records[prev].isMoreThan(records[j])) {
+                change(records, prev, j)
             }
         }
-        val time = System.currentTimeMillis() - startTime
-        if (time > maxTimeInMs) return true
     }
-
-    return false
 }
 
-private fun change(lines: MutableList<String>, i: Int, j: Int) {
-    val tmp = lines[i]
-    lines[i] = lines[j]
-    lines[j] = tmp
+private fun change(records: MutableList<Record>, i: Int, j: Int) {
+    val tmp = records[i]
+    records[i] = records[j]
+    records[j] = tmp
 }
 
-private fun String.isMoreThan(line: String): Boolean {
-    val thisNameStart = this.indexOfFirst { it == ' ' } + 1
-    val secondNameStart = line.indexOfFirst { it == ' ' } + 1
-
-    val thisName = this.substring(thisNameStart)
-    val secondName = line.substring(secondNameStart)
-
-    return thisName.compareTo(secondName) > 0
+private fun Record.isMoreThan(record: Record): Boolean {
+    return this.name.compareTo(record.name) > 0
 }
-
