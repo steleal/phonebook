42cc25acf7a05110b09e05871f61cebce86a226d
steleal <steleal@yandex.ru>
2020-02-02 18:02:37 +0400

    Added bubbleSort wit stop

--- src/phonebook/Sort.kt
+++ src/phonebook/Sort.kt
@@ -1,7 +1,7 @@
 package phonebook
 
 internal fun bubbleSort(records: MutableList<Record>) {
-    if (records.size<2) return
+    if (records.size < 2) return
     for (i in records.indices) {
         val high = records.lastIndex - i
         var maxI = 0
@@ -14,16 +14,34 @@ internal fun bubbleSort(records: MutableList<Record>) {
     }
 }
 
+internal fun bubbleSortWithStop(records: MutableList<Record>, maxTimeInMs: Long): Boolean {
+    val startTime = System.currentTimeMillis()
+
+    if (records.size < 2) return false
+    for (i in records.indices) {
+        val high = records.lastIndex - i
+        var maxI = 0
+        for (j in 0..high) {
+            if (records[j].isMoreThan(records[maxI])) {
+                maxI = j
+            }
+        }
+        swap(records, maxI, high)
+        if (System.currentTimeMillis() - startTime > maxTimeInMs) return true
+    }
+    return false
+}
+
 internal fun quickSort(records: MutableList<Record>) {
-    if (records.size<2) return
+    if (records.size < 2) return
     quickSort(records, 0, records.lastIndex)
 }
 
 private fun quickSort(records: MutableList<Record>, low: Int, high: Int) {
     if (low < high) {
-        val pivotIndex = partition(records, low, high )
-        quickSort(records, low, pivotIndex-1)
-        quickSort(records,pivotIndex+1,high)
+        val pivotIndex = partition(records, low, high)
+        quickSort(records, low, pivotIndex - 1)
+        quickSort(records, pivotIndex + 1, high)
     }
 
 }
@@ -31,7 +49,7 @@ private fun quickSort(records: MutableList<Record>, low: Int, high: Int) {
 private fun partition(records: MutableList<Record>, low: Int, high: Int): Int {
     val pivot = records[high]
     var i = low
-    for (j in low..high-1) {
+    for (j in low..high - 1) {
         if (records[j].name <= pivot.name) {
             swap(records, i, j)
             i++
@@ -42,7 +60,7 @@ private fun partition(records: MutableList<Record>, low: Int, high: Int): Int {
 }
 
 private fun swap(records: MutableList<Record>, i: Int, j: Int) {
-    if (i==j) return
+    if (i == j) return
     val tmp = records[i]
     records[i] = records[j]
     records[j] = tmp
