9e6bb8011cda48745f7000cb7593d1127c1223fe
steleal <steleal@yandex.ru>
2020-02-02 11:40:59 +0400

    Add util class PhoneBooks.kt

---
+++ src/phonebook/PhoneBooks.kt
@@ -0,0 +1,29 @@
+package phonebook
+
+import java.io.File
+
+fun importRecordsFromFile(name: String): List<Record> {
+    val file = File(name)
+    val records = mutableListOf<Record>()
+
+    file.forEachLine { line ->
+        val record = lineToRecord(line)
+        record?.let { records.add(record) }
+    }
+
+    return records
+}
+
+private fun lineToRecord(line: String): Record? {
+    if (line.isBlank() || !line.contains(" ")) return null
+
+    val numberEnd = line.indexOfFirst { it == ' ' }
+    val nameStart = numberEnd + 1
+
+    val number = line.substring(0, numberEnd)
+    val name = line.substring(nameStart)
+
+    return Record(number, name)
+}
+
+fun importLinesFromFile(name: String) = File(name).readLines()
