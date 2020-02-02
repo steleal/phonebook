9ceb66d54ef849e1d476c7ea9a9b8365dc41c560
steleal <steleal@yandex.ru>
2020-02-02 14:36:04 +0400

    Implemented QuickSort with Partition Lomuto

--- src/phonebook/Sort.kt
+++ src/phonebook/Sort.kt
@@ -6,17 +6,41 @@ internal fun bubbleSort(records: MutableList<Record>) {
         for (j in 1..(records.lastIndex - i)) {
             val prev = j - 1
             if (records[prev].isMoreThan(records[j])) {
-                change(records, prev, j)
+                swap(records, prev, j)
             }
         }
     }
 }
 
 internal fun quickSort(records: MutableList<Record>) {
-    TODO()
+    if (records.size<2) return
+    quickSort(records, 0, records.lastIndex)
 }
 
-private fun change(records: MutableList<Record>, i: Int, j: Int) {
+private fun quickSort(records: MutableList<Record>, low: Int, high: Int) {
+    if (low < high) {
+        val pivotIndex = partition(records, low, high )
+        quickSort(records, low, pivotIndex-1)
+        quickSort(records,pivotIndex+1,high)
+    }
+
+}
+
+private fun partition(records: MutableList<Record>, low: Int, high: Int): Int {
+    val pivot = records[high]
+    var i = low
+    for (j in low..high-1) {
+        if (records[j].name <= pivot.name) {
+            swap(records, i, j)
+            i++
+        }
+    }
+    swap(records, i, high)
+    return i
+}
+
+private fun swap(records: MutableList<Record>, i: Int, j: Int) {
+    if (i==j) return
     val tmp = records[i]
     records[i] = records[j]
     records[j] = tmp
