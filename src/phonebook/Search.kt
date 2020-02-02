2a449ca29902e645171eff9e5f8564bbd4f0a0d9
steleal <steleal@yandex.ru>
2020-02-02 13:58:40 +0400

    Implemented binarySearch

--- src/phonebook/Search.kt
+++ src/phonebook/Search.kt
@@ -30,5 +30,26 @@ internal fun jumpSearch(records: List<Record>, searchName: String): Record? {
 }
 
 internal fun binarySearch(records: List<Record>, searchName: String): Record? {
-    TODO()
+    if (records.isEmpty()) return null
+    if (records.first().name>searchName) return null
+    if (records.last().name<searchName) return null
+
+    var first = 0
+    var last = records.lastIndex
+
+    while ( first<last) {
+        val middle = first + (last - first) / 2
+
+        if (searchName <= records[middle].name) {
+            last = middle
+        } else {
+            first = middle + 1
+        }
+    }
+
+    if (records[last].name == searchName) {
+        return records[last]
+    } else {
+        return null
+    }
 }
