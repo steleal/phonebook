57c5960cbc0b2755b184f48c24643b5f745075cd
steleal <steleal@yandex.ru>
2020-02-04 22:23:59 +0400

    Set Record as Comparable

--- src/phonebook/Record.kt
+++ src/phonebook/Record.kt
@@ -1,3 +1,8 @@
 package phonebook
 
-class Record(val phoneNumber: String, val name: String)
+class Record(val phoneNumber: String, val name: String) : Comparable<Record>{
+
+    override operator fun compareTo(record: Record): Int {
+        return this.name.compareTo(record.name)
+    }
+}
